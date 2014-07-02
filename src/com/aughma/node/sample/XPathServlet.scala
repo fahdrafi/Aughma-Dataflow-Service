6fa1b5b928660cd439a3c67c1fd7b2d0dcc8ec89
Fahd Rafi <fahd.rafi@gmail.com>
2014-07-02 15:28:11 +0100

    XPath updates

--- src/com/aughma/node/sample/XPathServlet.scala
+++ src/com/aughma/node/sample/XPathServlet.scala
@@ -2,8 +2,8 @@ package com.aughma.node.sample
 
 import javax.servlet.http._
 import scala.xml.XML
-import scala.io.Codec
-import scala.xml.Elem
+import scala.xml.Node
+import scala.xml.NodeSeq
 
 class XPathServlet extends AughmaServlet {
 	override def doGet(req: HttpServletRequest, resp: HttpServletResponse) = {
@@ -21,18 +21,24 @@ class XPathServlet extends AughmaServlet {
 		resp.getWriter().print(nodeDescription)
 	}
 
-//	def XPath(xmlElement: Elem, xPaths: List[String]): Any = {
-//	  xPaths match {
-//	    case Nil => None
-//	    case head :: tail => Some(List(XPath(xmlElement, head))++XPath(xmlElement, tail))
-//	  }
-//	}
-//	
-//	def XPath(xmlElement: Elem, xPath: String): Any = {
-//	  xPath match {
-//	    case x if "[^/]*//".r.findFirstIn(x).isDefined =>  
-//	  }
-//	}
+    def XPath(xmlNode: Node, xPath: String) : NodeSeq = {
+      xPath match {
+        case "" => NodeSeq.Empty
+        case p if p.startsWith("//") && !p.substring(2).contains('/') => xmlNode \\ p.substring(2)
+        case p if p.startsWith("/") && !p.substring(1).contains('/') => xmlNode \ p.substring(1)
+        case p if p.startsWith("//") => XPath(xmlNode \\ p.substring(2, p.indexOf('/', 2)), p.substring(p.indexOf('/', 2)))
+        case p if p.startsWith("/") => XPath(xmlNode \ p.substring(1, p.indexOf('/', 1)), p.substring(p.indexOf('/', 1)))
+        case p if p == xmlNode.label => xmlNode
+        case p => throw new Exception("xPath string start with '/' or '//'")
+      }
+	}
+	
+	def XPath(xmlSeq: NodeSeq, xPath: String) : NodeSeq = {
+	  xmlSeq match {
+	    case x: Node => throw new Exception("This call should have gone to the other implementation of XPath")
+	    case x => NodeSeq.fromSeq(xmlSeq.theSeq.map(n => XPath(n, xPath).theSeq).flatten)
+	  }
+	}
 	
 	override def doPost(req: HttpServletRequest, resp: HttpServletResponse) = {
 	  val reqBody = getRequestBody(req)
