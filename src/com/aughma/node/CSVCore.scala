34184ebee2412143105da501c4d8776b7b779dc9
Fahd Rafi <fahd.rafi@gmail.com>
2014-07-10 14:32:36 +0100

    CSV data parsing node

--- src/com/aughma/node/CSVCore.scala
+++ src/com/aughma/node/CSVCore.scala
@@ -1,10 +1,42 @@
 package com.aughma.node
 
+import javax.servlet.http._
 import scala.xml._
 
 class CSVCore extends NodeCore {
-	val description = <NodeDescription></NodeDescription> 
+	val description =
+		<NodeCore>
+			  <Stateful>false</Stateful>
+			  <Description>
+			  	This node takes a csv with headers and provides all rows as the output data.
+			  </Description>
+			  <Input type="string" name="csv" />
+			  <Output type="dynamic" name="data"/>
+		</NodeCore>
+	  
+	  
 	def ProcessInput(input: NodeSeq): NodeSeq = {
-		NodeSeq.Empty
+	  val csv = (input\\"csv").theSeq.head.text
+	  val xpath = (input\\"xpath").theSeq.head.text
+	  
+	  val lines = csv
+	    .split(List('\r', '\n').toArray)
+	    .filter(_.length > 0)
+
+	  val headers = lines
+	  	.head
+	  	.split(',')
+	  	
+	  val data = lines
+	  	.tail
+	  	.map(_.split(','))
+	  	.map(_.zip(headers).toList)
+	  	.toSeq
+	 
+	  <Output/>.copy(child = data.map(record => {
+	    <dynamic/>.copy(child = record.map(item => <a>{item._2}</a>.copy(label = item._1)))
+	  }))
+	  	
 	}
+	
 }
--- src/com/aughma/node/NodeAdaptor.scala
+++ src/com/aughma/node/NodeAdaptor.scala
@@ -1,5 +1,7 @@
 package com.aughma.node
 
+import javax.servlet.http._
+
 
 class NodeAdaptor extends AughmaServlet {
   
