5be410878a526f3a28315a224f027d2caf58b3e6
Fahd Rafi <fahd.rafi@gmail.com>
2014-07-23 22:27:22 +0100

    Added basic Node Adaptor
    Still needs to be fleshed out and completed

--- src/com/aughma/node/NodeAdaptor.scala
+++ src/com/aughma/node/NodeAdaptor.scala
@@ -1,7 +1,36 @@
 package com.aughma.node
 
 import javax.servlet.http._
+import scala.xml._
+import com.google.appengine.api.taskqueue._
+import com.google.appengine.api.taskqueue.TaskOptions.Builder._
 
+/**
+ * Node Adaptor is responsible for:
+ * 1. Accepting an input pushed to it.
+ * 2. Storing it in a queue
+ * 3. Managing execution according to the defined parameters (parallelism etc)
+ * 4. Connecting the output to another Node adaptor (if need be) 
+ */
+trait NodeAdaptor extends AughmaServlet {
+	val description: NodeSeq
+	def PostMessage(input: NodeSeq): NodeSeq = {
+		val queue = QueueFactory.getDefaultQueue();
+        queue.add(withUrl("/pathToNodeCore").param("key", "value"));
+        <MessageSent/>
+	}
+	
+	final override def doGet(req: HttpServletRequest, resp: HttpServletResponse) = {
+      assert(!description.isEmpty, "Must provide description for AughmaServlet: "+ this.getClass().getName())
+      
+      resp.setContentType("application/xml")
+      resp.getWriter().print(description)
+    }
 
-class NodeAdaptor extends AughmaServlet {
+	final override def doPost(req: HttpServletRequest, resp: HttpServletResponse) = {
+		val requestBody = XML.loadString(getRequestBody(req))
+		
+		resp.setContentType("application/xml")
+		resp.getWriter().print(PostMessage(requestBody))
+	}
 }
