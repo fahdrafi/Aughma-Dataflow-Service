f638e548170f9c683301240cf06ae148406c8fce
Fahd Rafi <fahd.rafi@gmail.com>
2014-11-28 12:13:38 +0000

    Strings to constants

--- src/com/aughma/library/FetchURLBlock.scala
+++ src/com/aughma/library/FetchURLBlock.scala
@@ -5,11 +5,13 @@ import scala.io.Source
 import java.util.logging.Level
 
 object FetchURLBlock extends TransformBlock {
+  val INPUTURL = "url"
+  val OUTPUTRESPONSE = "Response"
   val errorPort = new LogOutputPort("ERROR", Level.SEVERE)
-  val inputPorts = Map("url"->new ConstantInputPort("http://www.google.com"))
-  val outputPorts = Map("Response" -> new LogOutputPort("FetchURL-Response"))
+  val inputPorts = Map(INPUTURL->new ConstantInputPort("http://www.google.com"))
+  val outputPorts = Map(OUTPUTRESPONSE -> new LogOutputPort("FetchURL-Response"))
   override def Transform(params: Map[String, String]): Map[String, String] = {
-	  val url = params("url")
-	  Map("Response"->"Some response from server")
+	  val url = params(INPUTURL)
+	  Map(OUTPUTRESPONSE->"Some response from server")
 	}
 }
